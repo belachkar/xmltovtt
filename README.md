359a1b658a301510a29a588df757a374fb6de3d8
belachkar <belachkar_ali@hotmail.com>
2023-02-14 09:42:07 +0100

    Add README, refactoring

---
+++ README.md
@@ -0,0 +1,1 @@
+# XML to VTT Converter
--- main.dart
+++ main.dart
@@ -3,9 +3,15 @@ import 'dart:io';
 import 'models/models.dart';
 
 void main(List<String> args) async {
-  final Directory dir = Directory('.');
+  Directory dir = Directory.current;
 
-  _checkDirExists(dir);
+  for (var i = 0; i < args.length; i++) {
+    if (args[i] == '-d' && args.length > i) {
+      dir = Directory(args[i + 1]);
+      _checkDirExists(dir);
+    }
+  }
+
   final fsEntities = await dir.list().toList();
   final xmlFsEntities = _getXMLFiles(fsEntities);
   final xmlFiles = _parseXMLFiles(xmlFsEntities);
@@ -15,12 +21,18 @@ void main(List<String> args) async {
 
 void _createVttFiles(Iterable<VttFile> vttFiles) {
   for (var vttFile in vttFiles) {
+    if (vttFile.items.length < 1) {
+      print('Skeped "${vttFile.path}": no parsed content.');
+      continue;
+    }
+
     final file = File(vttFile.path);
 
     String contents = 'WEBVTT \n\n';
     for (var item in vttFile.items) {
       contents += '${item.st} --> ${item.et}\n${item.sub}\n\n';
     }
+
     file.writeAsStringSync(contents);
   }
 }
@@ -37,8 +49,8 @@ List<XmlFile> _parseXMLFiles(Iterable<FileSystemEntity> xmlFsEntities) {
     final subMatches = RegExp(r'CDATA\[(.*)]]').allMatches(content).toList();
 
     if (stMatches.length == etMatches.length &&
-        etMatches == subMatches.length) {
-      print('Parsed success - ${subMatches.length} lines.');
+        etMatches.length == subMatches.length) {
+      print('Parsed success - ${subMatches.length} Cues.');
 
       final xmlFile = XmlFile([], path: file.path);
       for (var i = 0; i < subMatches.length; i++) {
@@ -51,7 +63,12 @@ List<XmlFile> _parseXMLFiles(Iterable<FileSystemEntity> xmlFsEntities) {
       xmlFiles.add(xmlFile);
     } else {
       print('! Warning: Parsing failed, an error when parsing');
-      print('\tThe number of parsed items (st, et, sub) do not match');
+      print('\tThe number of parsed lines (st, et, sub) do not match');
+      print({
+        'start time': '${stMatches.length} lines.',
+        'end time': '${etMatches.length} lines.',
+        'subtitles': '${subMatches.length} lines.',
+      });
     }
   }
 
@@ -61,11 +78,8 @@ List<XmlFile> _parseXMLFiles(Iterable<FileSystemEntity> xmlFsEntities) {
 List<FileSystemEntity> _getXMLFiles(List<FileSystemEntity> fsEntities) {
   final xmlFsEntities =
       fsEntities.where((entity) => entity.path.endsWith('.xml'));
-  print('${xmlFsEntities.length} files found:');
 
-  for (var file in xmlFsEntities) {
-    print(file.path);
-  }
+  print('${xmlFsEntities.length} files found:');
   return xmlFsEntities.toList();
 }
 
